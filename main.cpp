a616356dcafbe32d75801645a3cf4a7ef6f0dd11
Ahmed <sellaoui.ahmed213@gmail.com>
2026-02-26 22:11:07 +0000

    init commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num1 ;
+    char ch ;
+    long long num2 ;
+    float num3 ;
+    double num4 ;
+
+    cin >> num1 >> num2 >> ch >> num3 >> num4;
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << ch << endl;
+    cout << num3 << endl;
+    cout << num4 << endl;
+}
