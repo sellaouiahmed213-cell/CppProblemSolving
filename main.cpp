e2d9eb8d9796539a9ac5362c1ce00f56fe39712e
Ahmed <sellaoui.ahmed213@gmail.com>
2026-02-27 20:42:27 +0000

    init commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main() {
 
-    int num1 ;
-    char ch ;
-    long long num2 ;
-    float num3 ;
-    double num4 ;
+    long long X, Y;
+    cin >> X >> Y;
 
-    cin >> num1 >> num2 >> ch >> num3 >> num4;
-    cout << num1 << endl;
-    cout << num2 << endl;
-    cout << ch << endl;
-    cout << num3 << endl;
-    cout << num4 << endl;
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+    cout << X << " - " << Y << " = " << X - Y << endl;
+
+    return 0;
 }
